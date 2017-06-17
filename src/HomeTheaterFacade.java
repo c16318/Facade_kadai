3f5667dacc3809c9cc275ee0f0ce94f19de5487b
matsu <matsu@DESKTOP-VB6PH3E>
2017-06-17 21:02:22 +0900

    新規

---
+++ src/HomeTheaterFacade.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author matsu
+ */
+public class HomeTheaterFacade {
+    public static void play(){
+        Amplifier amp = new Amplifier();
+        BlurayPlayer bluray = new BlurayPlayer();
+        Projector projector = new Projector();
+        Screen screen = new Screen();
+    }
+}
---
+++ src/HomeTheaterTestDrive.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * @author matsu
+ */
+public class HomeTheaterTestDrive {
+    public static void main(String[] args){
+        HomeTheaterFacade.play();
+    }
+}
